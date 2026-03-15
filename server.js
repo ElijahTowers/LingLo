d121e104de8001861f343e5333417318d14851fe
Lowie <lowie@mac-mini.local>
2026-03-15 17:52:27 +0100

    Add word list filters and sorting

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const APP_VERSION = 'v4.82';
+const APP_VERSION = 'v4.83';
 const express = require('express');
 const crypto = require('crypto');
 const multer = require('multer');
