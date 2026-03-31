108992a21c9ddcbd9bfc448f7ca24e6ba9a6671a
Lowie <lowie@mac-mini.local>
2026-03-31 20:10:45 +0200

    Shrink translation popup to a minimal bubble

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const APP_VERSION = 'v4.94';
+const APP_VERSION = 'v4.95';
 const express = require('express');
 const crypto = require('crypto');
 const multer = require('multer');
